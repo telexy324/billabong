6612daa203a69670e1a86048e75200954f4603f6
Yang Xu <yangxu@YangdeMacBook-Pro.local>
2025-03-24 05:22:58 +0800

    032401

--- model/tool.go
+++ model/tool.go
@@ -13,23 +13,32 @@ type Tool struct {
 	Description string   `json:"description"`
 	Downloads   int      `json:"downloads"`
 	Enabled     bool     `json:"enabled"`
-	FilesRaw    string   `gorm:"default:'[]'" json:"-"`
+	FileIdsRaw  string   `gorm:"default:'[]'" json:"-"`
 	Files       []Upload `gorm:"-" json:"files"`
 }
 
 func (m *Tool) BeforeSave(tx *gorm.DB) error {
+	if m.Files != nil && len(m.Files) > 0 {
+		fileIds := make([]uint64, 0, len(m.Files))
+		for _, file := range m.Files {
+			fileIds = append(fileIds, file.ID)
+		}
+	}
 	if data, err := json.Marshal(m.Files); err != nil {
 		return err
 	} else {
-		m.FilesRaw = string(data)
+		m.FileIdsRaw = string(data)
 	}
 	return nil
 }
 
 func (m *Tool) AfterFind(tx *gorm.DB) error {
-	if err := json.Unmarshal([]byte(m.FilesRaw), &m.Files); err != nil {
+	fileIds := make([]uint64, 0)
+	if err := json.Unmarshal([]byte(m.FileIdsRaw), &fileIds); err != nil {
 		return err
 	}
-
+	if err := tx.Model(&Upload{}).Where("id in (?)", fileIds).Find(&m.Files).Error; err != nil {
+		return err
+	}
 	return nil
 }
